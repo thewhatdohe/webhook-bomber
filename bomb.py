f31a53c41e222def2d9b003985db545f80407dda
thewhatdohe <157780326+thewhatdohe@users.noreply.github.com>
2026-04-22 10:31:12 +0400

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,11 +1,11 @@
 import requests
 
-webhook-url = "insert url here"
-message-data = {
+url = "insert url here"
+data = {
   "content": "insert any text you want",
   "username": "insert username"
 }
 
 print("bombing has begun")
 while True:
-  response = requests.post(webhook-url, json=data)
+  response = requests.post(url, json=data)
